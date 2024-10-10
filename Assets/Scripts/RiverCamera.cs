5385e019a7cc1cc7165a1536793dad6e947cdaf8
outcast36 <wyattayers43@gmail.com>
2024-10-10 16:14:55 -0700

    River level camera panning script

--- Assets/Scripts/RiverCamera.cs
+++ Assets/Scripts/RiverCamera.cs
@@ -7,24 +7,24 @@ public class RiverCamera : MonoBehaviour
 {
     private double PanAngle=Math.PI/2; // Angle in radians of positive x-axis in [0 pi/2]
     private Vector3 PanEndPos;
+    private bool panned=false;
     private float timer=0F;
     [SerializeField]
     private float PanStartTime=10F;
     [SerializeField]
-    private float PanDuration=1F;
+    private float PanDuration=5F;
 
     void Start(){
-        PanEndPos = transform.position+((float) Math.Sin(PanAngle)*Vector3.up)+((float)Math.Cos(PanAngle)*Vector3.right);
+        PanEndPos = new Vector3(10,10,0);//transform.position+((float) Math.Sin(PanAngle)*Vector3.up)+((float)Math.Cos(PanAngle)*Vector3.right);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (timer<PanStartTime) timer+=Time.deltaTime;
-        print(timer);
-        if (timer>=PanStartTime){
-            print("Starting pan");
+        if (timer>=PanStartTime && !panned){
             StartCoroutine(LerpFromTo(transform.position, PanEndPos, PanDuration));
+            panned=true;
         }
     }
 
