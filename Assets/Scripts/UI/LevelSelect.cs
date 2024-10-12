d301a9b87290e2e3d52c482d529d9d2191173ca2
milaeris <milaeris@uw.edu>
2024-10-11 19:07:25 -0700

    added new sprites to level menu

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -10,17 +10,6 @@ public class LevelSelect : MonoBehaviour
     private string curStage;
     public FishButton[] fishButtons;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SelectStage(FishButton stage)
     {
