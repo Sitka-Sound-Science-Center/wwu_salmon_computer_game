fc67a27fb2a52f7bfefe158f191d74b6860a6392
Steven Grubb <sgrubb314@gmail.com>
2024-10-16 14:10:23 -0700

    Added pause menu to river

--- Assets/Scripts/Steven/ButtonMovement.cs
+++ Assets/Scripts/Steven/ButtonMovement.cs
@@ -10,26 +10,45 @@ public class ButtonMovement : MonoBehaviour
     float MoveSpeed = 30;
     float smooth = 5.0f;
     float tiltAngle = 60.0f;
+    Vector3 scale;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(horizontalInput, verticalInput,0) * MoveSpeed * Time.deltaTime);
+        
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        if(horizontalInput < 0)
+        {
+            transform.localScale = -scale;
+        }
+        else
+        {
+            transform.localScale = scale;
+        }
+
+
+
+
+
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * MoveSpeed * Time.deltaTime);
+        float tiltAroundZ = Mathf.Atan2(verticalInput, horizontalInput) * Mathf.Rad2Deg;
+        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+        transform.SetPositionAndRotation(transform.position, target);
         // Smoothly tilts a transform towards a target rotation.
-        float tiltAroundZ = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg;
+        
         // float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
 
 
         // Rotate the cube by converting the angles into a quaternion.
-        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+        
 
         // Dampen towards the target rotation
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
