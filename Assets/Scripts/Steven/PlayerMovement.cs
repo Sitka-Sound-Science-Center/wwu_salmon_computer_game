f8c44e0b497c2fb30bed75ad3dec86cac0d90b67
Steven Grubb <sgrubb314@gmail.com>
2024-10-16 20:53:54 -0700

    OnscreenJoystick works, use the square in the bottom left corner of the river scene

--- Assets/Scripts/Steven/PlayerMovement.cs
+++ Assets/Scripts/Steven/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class PlayerMovement : MonoBehaviour
     float MoveSpeed = 30;
     float horizontalInput = 0;
     float verticalInput = 0;
+    public InputControl control;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * MoveSpeed * Time.deltaTime);
