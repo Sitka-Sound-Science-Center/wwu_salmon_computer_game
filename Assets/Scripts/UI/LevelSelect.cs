d0f2bbda62f119c0b5b373e8ca38ec4127d3ec1b
outcast36 <wyattayers43@gmail.com>
2024-10-18 11:16:12 -0700

    ChooseSelectedLevel returns string

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -3,50 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class LevelSelect : MonoBehaviour
+public class LevelSelect : MonoBehaviour 
 {
     public GameObject currentInfo;
 
     public TextMeshProUGUI curSpecies;
-    private string curStage;
     public FishButton[] fishButtons;
 
     public LoadScene loadScene;
 
 
-    public void SelectStage(FishButton stage)
-    {
-        curStage = stage.name;
-        ChooseSelectedLevel();
-        foreach (FishButton fish in fishButtons)
-        {
+    public void SelectStage(FishButton stage) {
+        ChooseSelectedLevel(stage.name);
+        foreach (FishButton fish in fishButtons) {
             fish.SetSelect(stage == fish);
         }
     }
 
-    public void SelectSpecies(string species)
-    {
+    public void SelectSpecies(string species) {
         curSpecies.text = species;
     }
 
-    public void ChooseSelectedLevel()
-    {
-        if (curStage == "Eggs" || curStage == "Alevin"
-            || curStage == "Fry" || curStage == "Smolt")
-        {
+    public string ChooseSelectedLevel(string curStage) {
+        if (curStage == "Alevin" || curStage == "Fry" || curStage == "Smolt") {
             loadScene.sceneToLoad = LoadScene.Level.River;
         } 
-        else if (curStage == "Adult")
-        {
+        else if (curStage == "Adult") {
             loadScene.sceneToLoad = LoadScene.Level.Ocean;
         } 
-        else if (curStage == "Spawning")
-        {
+        else if (curStage == "Spawning") {
             loadScene.sceneToLoad = LoadScene.Level.Spawning;
         } 
-        else
-        {
-            Debug.Log("Invalid level");
-        }
+        return loadScene.sceneToLoad.ToString();
     }
 }
--- Assets/Scripts/UI/LoadScene.cs
+++ Assets/Scripts/UI/LoadScene.cs
@@ -6,22 +6,18 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
     public Level sceneToLoad;
-    public enum Level
-    {
+    public enum Level {
         LevelSelect,
         River,
         Ocean,
         Spawning
     }
 
-    public void StartLoad()
-    {
+    public void StartLoad() {
         SceneManager.LoadScene(sceneToLoad.ToString());
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
-    {
+    public void OnTriggerEnter2D(Collider2D other) {
         SceneManager.LoadScene(sceneToLoad.ToString());
     }
 }
-
