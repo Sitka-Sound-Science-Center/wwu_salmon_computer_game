fa6ca058e9c19e8825a981d7f1499c3fc2251204
Steven Grubb <sgrubb314@gmail.com>
2024-10-23 21:22:02 -0700

    Adding boot anim and rock crushing

---
+++ Assets/Scripts/Steven/PebbleCrusher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PebbleCrusher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pebble"))
+        {
+            CircleCollider2D component;
+            if (collision.TryGetComponent(out component))
+            {
+                //if pebble and has collider2D
+                //disable just the circle collider, NOT the polygon collider
+                component.enabled = false;
+            }
+        }
+    }
+}
