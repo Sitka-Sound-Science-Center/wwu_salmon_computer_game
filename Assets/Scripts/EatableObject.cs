4a841e013a69d107b314dd389f899a045b369005
outcast36 <wyattayers43@gmail.com>
2024-11-02 19:31:47 -0700

    Fix hunger overflow

--- Assets/Scripts/EatableObject.cs
+++ Assets/Scripts/EatableObject.cs
@@ -8,18 +8,20 @@ public class EatableObject : MonoBehaviour
     private float RestoreValue=0.125F; // what proportion of the meter is refilled, in [0,1]
     [SerializeField]
     private GameObject HungerMeter; 
+    private RectTransform rt;
     private float MaxFill=550; // actual width of parent container
     private float ActualRestore;
 
     void Start() {
         ActualRestore = RestoreValue*MaxFill;
+        rt = HungerMeter.GetComponent<RectTransform>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player")) {
-            RectTransform rt = HungerMeter.GetComponent<RectTransform>();
+        if (other.gameObject.CompareTag("Player") && rt.rect.width!=MaxFill) {
             float curWidth = rt.rect.width;
-            float nextWidth = curWidth+ActualRestore;
+            // Cap hunger at max length of parent container
+            float nextWidth = System.Math.Min(MaxFill, curWidth+ActualRestore);
             rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, nextWidth);
         }
     }
