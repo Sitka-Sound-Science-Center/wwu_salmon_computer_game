b9eceabdb0a7befa5ee6ce59ffdeb73166b2a2ca
outcast36 <wyattayers43@gmail.com>
2024-11-12 15:06:45 -0800

    Hungry bar depletes over time (serialized duration)

--- Assets/Scripts/UI/HungerMeter.cs
+++ Assets/Scripts/UI/HungerMeter.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class HungerMeter : MonoBehaviour
 {
     [SerializeField] 
-    private float DepletionSpeed=0.125F; // in [0,1], is the amount the bar decreases each frame
+    private float EmptyTime=10F; // time in seconds needed for bar to go from full to empty
+    private float MaxFill=550; // actual width of parent container
+    private float timer=0F;
+    private RectTransform rt;
+
+    void Start() {
+        rt = gameObject.GetComponent<RectTransform>();
+    }
 
     // Update is called once per frame
     void Update() {
-        RectTransform rt = gameObject.GetComponent<RectTransform>();
         float curWidth = rt.rect.width;
-        // not sure how to update this so the comment on DepletionSpeed is observed behavior
-        //float nextWidth = curWidth*(1-DepletionSpeed);
-        //rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, nextWidth);
+        timer+=Time.deltaTime;
+        if (curWidth>=0) { // no underflow on resources
+            float nextWidth = System.Math.Max(0,curWidth-(MaxFill*(Time.deltaTime/EmptyTime)));
+            // why cant it just be as simple as calling max() ???
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, nextWidth);
+        }
+        if (timer>=2.5F) {
+            print("Current width: " + rt.rect.width);
+            timer=0F;
+        }
     }
 }
