e45b83a6acf2f9955efade2d002011421c675658
outcast36 <wyattayers43@gmail.com>
2024-11-18 21:07:54 -0800

    Clean up spawn point script

--- Assets/Scripts/RiverSpawning.cs
+++ Assets/Scripts/RiverSpawning.cs
@@ -4,22 +4,9 @@ using UnityEngine;
 
 public class RiverSpawning : MonoBehaviour
 {
-    public GameObject AlevinSpawn;
     public GameObject FrySpawn;
     public GameObject SmoltSpawn;
     public GameObject Player;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void Spawn(string stage) {
         GameObject active;
