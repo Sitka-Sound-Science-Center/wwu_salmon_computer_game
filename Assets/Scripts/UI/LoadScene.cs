ca541979ee4b11d2f49f6614e1c0559cbf8df18c
outcast36 <wyattayers43@gmail.com>
2024-11-18 22:58:57 -0800

    fry and smolt select

--- Assets/Scripts/UI/LoadScene.cs
+++ Assets/Scripts/UI/LoadScene.cs
@@ -8,20 +8,13 @@ using TMPro;
 public class LoadScene : MonoBehaviour
 {
     public Level sceneToLoad;
-
+    public Stage stageToLoad;
     public GameObject loadingScreen;
+    public GameObject Camera;
     public Slider progressBar;
     public TextMeshProUGUI message;
-
+    public RiverSpawning sp;
     AsyncOperation loadingOperation;
-
-    void Start()
-    {
-        if (loadingScreen != null)
-        {
-            loadingScreen.SetActive(false);
-        }
-    }
 
     public enum Level {
         LevelSelect,
@@ -30,12 +23,34 @@ public class LoadScene : MonoBehaviour
         Spawning
     }
 
+    public enum Stage {
+        Alevin,
+        Fry,
+        Smolt,
+    }
+
+    void Start() {
+        if (loadingScreen != null) {
+            loadingScreen.SetActive(false);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        sp = GameObject.FindWithTag("RiverSpawns").GetComponent<RiverSpawning>();
+        Camera = GameObject.FindWithTag("Camera");
+        string stage = stageToLoad.ToString();
+        print(stage);
+        if (scene.name=="River"){
+            Vector3 PlayerPos = sp.Spawn(stage);
+            Camera.transform.position = new Vector3(PlayerPos.x, PlayerPos.y, -10);
+        } 
+    }
+
     public void StartLoad() {
         SceneManager.LoadScene(sceneToLoad.ToString());
     }
 
-    public void StartLoadAsync()
-    {
+    public void StartLoadAsync() {
         loadingScreen.SetActive(true);
         //message = loadingScreen.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
         //progressBar = loadingScreen.transform.GetChild(1).gameObject.GetComponent<Slider>();
@@ -43,34 +58,28 @@ public class LoadScene : MonoBehaviour
 
         loadingOperation = SceneManager.LoadSceneAsync(sceneToLoad.ToString());
         loadingOperation.allowSceneActivation = false;
+        SceneManager.sceneLoaded+=OnSceneLoaded;
         StartCoroutine(WaitForLoad());
     }
 
-    IEnumerator WaitForLoad()
-    {
+    IEnumerator WaitForLoad() {
         yield return new WaitForSeconds(0.2f);
-
         //progressBar.value = Mathf.Clamp01(loadingOperation.progress / 0.9f);
-
-        while (!loadingOperation.isDone)
-        {
-            if (loadingOperation.progress >= 0.9f && progressBar.value == 1)
-            {
+        while (!loadingOperation.isDone) {
+            if (loadingOperation.progress >= 0.9f && progressBar.value == 1) {
                 progressBar.value = 1;
                 message.text = "CLICK ANYWHERE TO CONTINUE";
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                {
+                if (Input.GetKeyDown(KeyCode.Mouse0)) {
                     loadingOperation.allowSceneActivation = true;
                 }
-            } else
-            {
+            } 
+            else {
                 // fake loading, makes progress bar look better when loading too fast
                 progressBar.value += 0.1f;
                 yield return new WaitForSeconds(0.05f);
             }
             yield return null;
         }
-
 
     }
 
