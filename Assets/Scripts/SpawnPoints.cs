822f42cd018b701dc7e481f0ce6b89095e0cc7f6
outcast36 <wyattayers43@gmail.com>
2024-11-20 20:10:09 -0800

    Rename river spawning code to be generic

--- Assets/Scripts/SpawnPoints.cs
+++ Assets/Scripts/SpawnPoints.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RiverSpawning : MonoBehaviour
+public class SpawnPoints : MonoBehaviour
 {
     public GameObject AlevinSpawn;
     public GameObject FrySpawn;
     public GameObject SmoltSpawn;
+    public GameObject AdultSpawn;
+    public GameObject SpawningSpawn;
     public GameObject Player;
+    private string[] stages = {"Alevin", "Fry", "Smolt", "Adult", "Spawning"}; // change to use the Stage enum?
 
-    // Helper to map string stage name to index
-    private int stageIndex(string stage) {
-        if (stage=="Alevin") return 0;
-        else if (stage=="Fry") return 1;
-        else return 2;
-    }
-
-    public Vector3 Spawn(string stage) {
-        int idx=stageIndex(stage); 
+    public Vector3 Spawn(string stage) { // take Stage enum as param?
+        int idx = System.Array.IndexOf(stages,stage); // mod player transform #childs eg adult prefab will only have one child
         GameObject active = Player.transform.GetChild(idx).gameObject;
         Vector3 PlayerPos, SpawnPos;
         SpawnPos = gameObject.transform.GetChild(idx).position; 
