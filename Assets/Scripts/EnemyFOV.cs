adb77c43c59cc0e0560e1a381fdcdbb6cdf6091f
outcast36 <wyattayers43@gmail.com>
2025-01-15 15:56:40 -0800

    Start on enemy FOV detection and pusuit

---
+++ Assets/Scripts/EnemyFOV.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFOV : MonoBehaviour
+{
+    public float DetectionRadius;
+    public float ViewAngle; // in radians
+}
