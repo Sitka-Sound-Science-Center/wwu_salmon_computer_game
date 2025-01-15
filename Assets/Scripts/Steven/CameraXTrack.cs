c3ea3e2759d730bfcf5dfb93e8e1a9b34478d60a
Milaeris <mila.eris@gmail.com>
2025-01-14 18:31:41 -0800

    added predators to ocean, updated death screens, added x bounds to camera

--- Assets/Scripts/Steven/CameraXTrack.cs
+++ Assets/Scripts/Steven/CameraXTrack.cs
@@ -8,19 +8,26 @@ public class CameraXTrack : MonoBehaviour
     float fryHeight;
 
     public bool trackY;
+    public bool boundX;
     public float topBound;
     public float lowBound;
     public float leftBound;
+    public float rightBound;
  
     void Update()
     {
         float yPos = this.transform.position.y;
+        float xPos = Mathf.Lerp(transform.position.x, Player.transform.position.x, 0.75f);
         if (trackY)
         {
             yPos = Mathf.Lerp(transform.position.y, Player.transform.position.y, 0.75f);
             yPos = Mathf.Clamp(yPos, lowBound, topBound);
         }
-        this.transform.position = new Vector3(Mathf.Lerp(transform.position.x, Player.transform.position.x, 0.75f), yPos, this.transform.position.z);   
+        if (boundX)
+        {
+            xPos = Mathf.Clamp(xPos, leftBound, rightBound);
+        }
+        this.transform.position = new Vector3(xPos, yPos, this.transform.position.z);   
     }
 
     public void ChangeToFryPosition()
