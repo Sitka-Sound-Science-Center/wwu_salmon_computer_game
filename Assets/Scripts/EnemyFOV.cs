bcff4d221d070b3c0c2e60de923309eb0c2794ed
outcast36 <wyattayers43@gmail.com>
2025-01-16 14:14:14 -0800

    Helper method check if player is within certain radius of enemy

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -7,17 +7,22 @@ public class EnemyFOV : MonoBehaviour
     public float DetectionRadius;
     public float ViewAngle; // in radians, breadth of FOV cone
     public LayerMask ObjectsMask;
+    public LayerMask PlayerMask; // layer that only has player object on it
 
     public Vector3 DirectionFromAngle(float angle) {
-        return new Vector3(Mathf.Cos(angle),Mathf.Sin(angle),0);
+        return new Vector3(Mathf.Cos(angle),Mathf.Sin(angle),0); // should this be vec2 or have trig values swapped?
     }
 
-    public void FindVisibleTargets() {
-        Collider2D[] ObjectsInViewRadius = Physics2D.OverlapCircleAll(gameObject.transform.position, DetectionRadius, ObjectsMask);
+    public bool IsPlayerInRadius() {
+        Collider2D PlayerCollider = Physics2D.OverlapCircle(gameObject.transform.position, DetectionRadius, PlayerMask);
+        return PlayerCollider == null;
     }
+
     public bool IsPlayerVisible(Vector3 ToPlayer) {
-        Vector3 LowestView = DirectionFromAngle(-ViewAngle/2);
-        Vector3 HighestView = DirectionFromAngle(ViewAngle/2);
+        if (!IsPlayerInRadius()) return false;
+        Collider2D[] ObjectsInViewRadius = Physics2D.OverlapCircleAll(gameObject.transform.position, DetectionRadius, ObjectsMask);
+        
+
         // ignore occlusion -- check if ray hits something else before player
         // if vector to player is facing away from vision cone return false
         // if vector to player is outside of vision cone return false
