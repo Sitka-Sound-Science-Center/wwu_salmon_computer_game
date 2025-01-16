ac8ff45c6d199e33902997d1432616322b483969
outcast36 <wyattayers43@gmail.com>
2025-01-16 14:03:28 -0800

    In progress enemy view stuff

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 public class EnemyFOV : MonoBehaviour
 {
     public float DetectionRadius;
-    public float ViewAngle; // in radians
+    public float ViewAngle; // in radians, breadth of FOV cone
+    public LayerMask ObjectsMask;
+
+    public Vector3 DirectionFromAngle(float angle) {
+        return new Vector3(Mathf.Cos(angle),Mathf.Sin(angle),0);
+    }
+
+    public void FindVisibleTargets() {
+        Collider2D[] ObjectsInViewRadius = Physics2D.OverlapCircleAll(gameObject.transform.position, DetectionRadius, ObjectsMask);
+    }
+    public bool IsPlayerVisible(Vector3 ToPlayer) {
+        Vector3 LowestView = DirectionFromAngle(-ViewAngle/2);
+        Vector3 HighestView = DirectionFromAngle(ViewAngle/2);
+        // ignore occlusion -- check if ray hits something else before player
+        // if vector to player is facing away from vision cone return false
+        // if vector to player is outside of vision cone return false
+        // otherwise return true 
+    }
 }
