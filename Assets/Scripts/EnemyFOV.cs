530b0fa38d30a5cffe072a1512d50f2cb84b5793
outcast36 <wyattayers43@gmail.com>
2025-01-16 16:08:32 -0800

    Convert to degrees and fix logic bug

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyFOV : MonoBehaviour
 {
     public float DetectionRadius;
-    public float ViewAngle; // in radians, breadth of FOV cone
+    public float ViewAngle; // in degrees, breadth of FOV cone
     public LayerMask ObjectsMask;
     public LayerMask PlayerMask; // layer that only has player object on it
 
@@ -17,18 +17,23 @@ public class EnemyFOV : MonoBehaviour
     // Check if player object is within the vision radius
     public bool IsPlayerInRadius() {
         Collider2D PlayerCollider = Physics2D.OverlapCircle(gameObject.transform.position, DetectionRadius, PlayerMask);
-        return PlayerCollider == null;
+        return PlayerCollider != null;
     }
 
     // Check if player object is inside the vision cone and enemy has line of sight to player
     public bool IsPlayerVisible() {
-        if (!IsPlayerInRadius()) return false;
+        print(gameObject.name);
+        if (!IsPlayerInRadius()) {
+            print("Player not in radius");
+            return false;
+        } 
+        print("Player entered radius: " + gameObject.name);
         GameObject Player = GameObject.FindWithTag("Player");
         Vector3 DirectionToPlayer = Player.transform.position - gameObject.transform.position;
+        print("Distance to player: " + DirectionToPlayer.magnitude + "(" + gameObject.name + ")");
         DirectionToPlayer.Normalize();
         // Check if player object is inside of the enemy's vision cone
-        float AngleToPlayer = Vector3.Angle(gameObject.transform.position.forward, DirectionToPlayer);
-        AngleToPlayer *= Mathf.Deg2Rad;
+        float AngleToPlayer = Vector3.Angle(gameObject.transform.forward, DirectionToPlayer);   
         if (AngleToPlayer > ViewAngle/2) return false;
         // Do a ray cast to check if player is occluded by something ? 
         print("Player in cone");
