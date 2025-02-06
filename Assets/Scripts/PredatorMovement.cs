bacece5427919bb8ba5aa74cea3ee6934467e0e7
outcast36 <wyattayers43@gmail.com>
2025-02-06 13:40:46 -0800

    Reverse scaling on sprite orientation

--- Assets/Scripts/PredatorMovement.cs
+++ Assets/Scripts/PredatorMovement.cs
@@ -72,12 +72,12 @@ public class PredatorMovement : MonoBehaviour
         if (position.x < transform.position.x)
         {
             //face sprite to the right
-            transform.localScale = new Vector3(scaleFactor.x, scaleFactor.y, scaleFactor.z);
+            transform.localScale = new Vector3(-scaleFactor.x, scaleFactor.y, scaleFactor.z);
         }
         else
         {
             //face sprite to the left
-            transform.localScale = new Vector3(-scaleFactor.x, scaleFactor.y, scaleFactor.z);
+            transform.localScale = new Vector3(scaleFactor.x, scaleFactor.y, scaleFactor.z);
         }
     }
 }
